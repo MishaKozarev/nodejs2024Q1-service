1c3b3d4a6a9300bffa2ee9e781010dd26e488bf8
MishaKozarev <mishaaaaa1986@mail.ru>
2024-03-10 20:11:19 +0300

    feat: add album service and controller

--- src/data-base/data-base.service.ts
+++ src/data-base/data-base.service.ts
@@ -2,10 +2,12 @@ import { Injectable } from '@nestjs/common';
 import { Artist } from 'src/data-base/entities/artist.entity';
 import { Track } from 'src/data-base/entities/track.entity';
 import { User } from 'src/data-base/entities/user.entity';
+import { Album } from './entities/album.entity';
 
 @Injectable()
 export class DataBaseService {
   public users: User[] = [];
   public tracks: Track[] = [];
   public artists: Artist[] = [];
+  public albums: Album[] = [];
 }

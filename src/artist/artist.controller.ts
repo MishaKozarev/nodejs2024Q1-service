b88f0f3aa4bab69dc80b723bb0d5ec667e7888ac
MishaKozarev <mishaaaaa1986@mail.ru>
2024-03-10 18:02:35 +0300

    feat: add artists endpoints

--- src/artist/artist.controller.ts
+++ src/artist/artist.controller.ts
@@ -1,4 +1,50 @@
-import { Controller } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Get,
+  Header,
+  Param,
+  ParseUUIDPipe,
+  Post,
+  Put,
+  UsePipes,
+  ValidationPipe,
+} from '@nestjs/common';
+import { ArtistService } from './artist.service';
+import { CreateArtistDto } from './dbo/createArtist.dbo';
+import { UpdateArtistDto } from './dbo/updateArtist.dbo';
+import { Artist } from './entities/artist.entity';
 
 @Controller('artist')
-export class ArtistController {}
+export class ArtistController {
+  constructor(private readonly artistService: ArtistService) {}
+
+  @Get()
+  @Header('Content-Type', 'application/json')
+  getArtistAll(): Artist[] {
+    return this.artistService.getArtistAll();
+  }
+
+  @Get(':id')
+  @Header('Content-Type', 'application/json')
+  getArtistById(@Param('id', ParseUUIDPipe) id: string): Artist {
+    return this.artistService.getArtistById(id);
+  }
+
+  @UsePipes(new ValidationPipe())
+  @Post()
+  @Header('Content-Type', 'application/json')
+  createArtistById(@Body() dto: CreateArtistDto): Artist {
+    return this.artistService.createArtistById(dto);
+  }
+
+  @UsePipes(new ValidationPipe())
+  @Put(':id')
+  @Header('Content-Type', 'application/json')
+  updateArtistById(
+    @Param('id', ParseUUIDPipe) id: string,
+    @Body() dto: UpdateArtistDto,
+  ): Artist {
+    return this.artistService.updateArtistById(id, dto);
+  }
+}
--- src/artist/artist.service.ts
+++ src/artist/artist.service.ts
@@ -1,4 +1,46 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
+import { v4 as uuidv4 } from 'uuid';
+import { CreateArtistDto } from './dbo/createArtist.dbo';
+import { UpdateArtistDto } from './dbo/updateArtist.dbo';
+import { Artist } from './entities/artist.entity';
 
 @Injectable()
-export class ArtistService {}
+export class ArtistService {
+  private artists: Artist[] = [];
+
+  public getArtistAll(): Artist[] {
+    return this.artists;
+  }
+
+  public getArtistById(id: string): Artist {
+    return this.findArtistById(id);
+  }
+
+  public createArtistById(dto: CreateArtistDto): Artist {
+    const artist: Artist = {
+      id: uuidv4(),
+      ...dto,
+    };
+    this.artists.push(artist);
+    return artist;
+  }
+
+  public updateArtistById(id: string, dto: UpdateArtistDto): Artist {
+    const artist = this.findArtistById(id);
+    return Object.assign(artist, dto);
+  }
+
+  public deleteArtistById(id: string): void {
+    const artist = this.findArtistById(id);
+    const artistIndex = this.artists.indexOf(artist);
+    this.artists.splice(artistIndex, 1);
+  }
+
+  private findArtistById(id: string): Artist {
+    const artist = this.artists.find((artist) => artist.id === id);
+    if (!artist) {
+      throw new NotFoundException(`Artist with ID = ${id} not found`);
+    }
+    return artist;
+  }
+}
